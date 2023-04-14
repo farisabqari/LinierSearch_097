fc42e52731fbb2de6875221bbe33900d3f39a993
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-04-14 16:25:23 +0800

    memasukann void Linesearch

--- LinierSearch_097.cpp
+++ LinierSearch_097.cpp
@@ -15,4 +15,22 @@ void input()
 			break;
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+	//accept array elements
+	cout << "\n--------------------\n";
+	cout << " Enter array elemetns \n";
+	cout << "----------------------\n";
+	for (i -= 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
+
+void linearsearch()
+{
+	char ch;
+	int comparisons; // number of comparisons
+
+	do
 	
